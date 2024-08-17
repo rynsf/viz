1d893b480166b3de49cca4189a3fe0eaf02520da
rynsf <rynsf@users.noreply.github.com>
2024-08-17 22:03:15 +0530

    Add vizVar

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ type data struct {
 }
 
 func maximum(arr []int, chOut chan data) int {
-	m := 1
+	m := -1
 	for _, n := range arr {
 		if n > m {
 			m = n
@@ -26,11 +26,31 @@ func maximum(arr []int, chOut chan data) int {
 	return m
 }
 
+func vizVar(val int) {
+	var boxSize float32 = 100
+	thick := boxSize / 10
+	rec := rl.Rectangle{
+		X:      0,
+		Y:      200,
+		Width:  boxSize,
+		Height: boxSize,
+	}
+	rl.DrawRectangleLinesEx(rec, thick, rl.RayWhite)
+	str := fmt.Sprintf("%d", val)
+	fontMeasure := rl.MeasureTextEx(font, str, 56, 0)
+	rl.DrawTextPro(font,
+		str,
+		rl.Vector2{X: rec.X + (boxSize / 2), Y: rec.Y + (boxSize / 2)},
+		rl.Vector2{X: fontMeasure.X / 2, Y: fontMeasure.Y / 2},
+		0,
+		56,
+		0,
+		rl.Green)
+}
 func vizArray(arr []int) {
 	var boxSize float32 = 100
 	thick := boxSize / 10
 	pad := -thick
-	rl.BeginDrawing()
 	for n := range arr {
 		rec := rl.Rectangle{
 			X:      (boxSize + pad) * float32(n),
@@ -50,7 +70,6 @@ func vizArray(arr []int) {
 			0,
 			rl.Green)
 	}
-	rl.EndDrawing()
 }
 
 func main() {
@@ -62,18 +81,22 @@ func main() {
 
 	arr := []int{5, 2, 8, 7, 9}
 	temparr := arr
+	tempm := -1
 	chOut := make(chan data)
 	timestart := rl.GetTime()
 	go maximum(arr, chOut)
 	for !rl.WindowShouldClose() {
-		if rl.IsKeyReleased(rl.KeyN) || rl.GetTime()-timestart > 2 {
+		if rl.IsKeyPressed(rl.KeyN) || rl.GetTime()-timestart > 2 {
 			fmt.Println("pressed next")
 			timestart = rl.GetTime()
-			temparr = (<-chOut).arr
+			temp := <-chOut
+			temparr = temp.arr
+			tempm = temp.m
 		}
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.GetColor(0x181818ff))
 		vizArray(temparr)
+		vizVar(tempm)
 		rl.EndDrawing()
 	}
 }
